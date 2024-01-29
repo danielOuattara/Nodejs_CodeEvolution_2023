a3e74cdeaeb5a082756cb68f30b92a476c873fdb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-01-29 17:54:32 +0100

    12 module wrapper IIFE

--- 12_module_wrapper/iife.js
+++ 12_module_wrapper/iife.js
@@ -7,3 +7,18 @@
   const superHero = "Superman";
   console.log(message, superHero);
 })("Greeting");
+
+(function (exports, require, module, __filename, __dirname, message) {
+  const superHero = "Mario";
+})("Greeting");
+
+(function (exports, require, module, __filename, __dirname, message) {
+  const superHero = "Mario";
+  console.log(message, superHero);
+  console.log("exports = ", exports);
+  console.log("require = ", require);
+  console.log("module = ", module);
+  console.log("__filename = ", __filename);
+  console.log("__dirname = ", __dirname);
+  console.log("__message = ", message);
+})("Greeting");
--- 12_module_wrapper/index.js
+++ 12_module_wrapper/index.js
@@ -1,2 +1,6 @@
 require("./batman");
 require("./superman");
+
+console.log("---------------------------------");
+console.log("module", module);
+console.log("--------------------------------");
